d863c2d3a9c1b5a8caa28c2d49f2acd2bcf35491
[mnergizci] <eemne@leeds.ac.uk>
2025-02-03 14:54:02 +0000

    integer checking in offset data

--- bin/LiCSBAS13_sb_inv.py
+++ bin/LiCSBAS13_sb_inv.py
@@ -509,6 +509,7 @@ def main(argv=None):
             ep1 = int(ifgd[:8])
             ep2 = int(ifgd[-8:])
             for skep in offsets:
+                ##making int
                 skep_int = int(skep.replace("-", ""))
                 if (ep1 <= int(skep_int)) and (ep2 > int(skep_int)):
                     coseismifgs.append(ifgd)
